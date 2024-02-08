02f910c81a3e16a75b0cc1bcc17771110f1b2257
Lancelot Robson <lancelot.robson@gmail.com>
2024-02-08 11:41:52 +0000

    Add calico-bpf setup script

---
+++ setup/cni-configs/calico-bpf.sh
@@ -0,0 +1,56 @@
+
+# Source : https://docs.tigera.io/calico/latest/operations/ebpf/install
+
+echo "Setup calico operator"
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml
+
+echo "Apply configmap"
+# Apply configmap to tell Operator what the real IP/port of the control plane is
+kubectl apply -f - <<EOF
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: kubernetes-services-endpoint
+  namespace: tigera-operator
+data:
+  KUBERNETES_SERVICE_HOST: \"$(kubectl cluster-info | grep 'control plane' | awk -F':|//' '{print $3}')\"
+  KUBERNETES_SERVICE_PORT: \"$(kubectl cluster-info | grep 'control plane' | awk -F':|//' '{print $4}')\"
+EOF
+
+# IPPool CIDR changed for RKE2 default 10.42.0.0/16
+echo "Setup calico custom resource"
+kubectl apply -f - <<EOF
+# This section includes base Calico installation configuration.
+# For more information, see: https://docs.tigera.io/calico/latest/reference/installation/api#operator.tigera.io/v1.Installation
+apiVersion: operator.tigera.io/v1
+kind: Installation
+metadata:
+  name: default
+spec:
+  # Configures Calico networking.
+  calicoNetwork:
+    linuxDataplane: BPF
+    hostPorts: null
+    # Note: The ipPools section cannot be modified post-install.
+    ipPools:
+    - blockSize: 26
+      cidr: 10.42.0.0/16
+      encapsulation: VXLANCrossSubnet
+      natOutgoing: Enabled
+      nodeSelector: all()
+    nodeAddressAutodetectionV4:
+          cidrs:
+            - '192.168.2.0/24'
+---
+# This section configures the Calico API server.
+# For more information, see: https://docs.tigera.io/calico/latest/reference/installation/api#operator.tigera.io/v1.APIServer
+apiVersion: operator.tigera.io/v1
+kind: APIServer
+metadata:
+  name: default
+spec: {}
+EOF
+
+echo "Patch Felixconfig"
+# Configure Calico to not try to clean up kube-proxy's iptables rules, since kube-proxy can't be disabled in RKE2
+kubectl patch felixconfiguration default --patch='{"spec": {"bpfKubeProxyIptablesCleanupEnabled": false}}'
