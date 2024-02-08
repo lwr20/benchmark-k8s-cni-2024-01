1d6e474bb182a52ad6791410e8fc492779008a62
Lancelot Robson <lancelot.robson@gmail.com>
2024-02-08 19:48:44 +0000

    Add bpf-vxlan-wg results

---
+++ results/calico-bpf-vxlan-9k-wg/1/dts-client.cmd
@@ -0,0 +1,1 @@
+kubectl exec -it cni-benchmark-a3 --  statexec -f dts-client.prom -d 10 -l id=calico-bpf-vxlan-9k-wg -l run=1 -i dts -mst 1704067200000 -dbc 11  -c 10.42.164.101 --  iperf3 -c 10.42.164.101 -O 1 -Z -t 60 --dont-fragment --json
